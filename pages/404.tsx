1d18d5b7cbd827c9c7b02e5d5274e0f071d572e5
adriancoll <acoll@aiudo.es>
2022-08-17 15:47:49 +0200

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,30 @@
+import { Box, Typography } from "@mui/material";
+import { ShopLayout } from "../components/layouts";
+
 // pages/404.tsx
 export default function Custom404() {
-    return (
-        <>
-            <h1>404 - Page Not Found</h1>
-        </>
-    )
+  return (
+    <ShopLayout
+      title="Página no encontrada"
+      pageDescription="No hay nada que mostrar aquí"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        sx={{
+          flexDirection: {
+            xs: "column",
+            sm: "row",
+          },
+        }}
+      >
+        <Typography variant="h1" component="h1" fontSize={100} fontWeight={200}>
+          404 |
+        </Typography>
+        <Typography marginLeft={2}>No hemos encontrado nada!</Typography>
+      </Box>
+    </ShopLayout>
+  );
 }
