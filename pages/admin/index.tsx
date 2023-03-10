e83390c816c4a46775dfb6ed3a885f222d44315f
Adrián coll <nege2002@gmail.com>
2023-03-10 13:53:00 +0100

    feat; dashboard tile component and fillec

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,18 @@
 import { NextPage } from 'next'
 import { AdminLayout } from '../../components/layouts'
-import { DashboardOutlined } from '@mui/icons-material'
+import {
+  AccessTimeOutlined,
+  AttachMoneyOutlined,
+  CancelPresentationOutlined,
+  CategoryOutlined,
+  CreditCardOffOutlined,
+  CreditCardOutlined,
+  DashboardOutlined,
+  GroupOutlined,
+  ProductionQuantityLimitsOutlined
+} from '@mui/icons-material'
+import { Card, CardContent, Grid, Typography } from '@mui/material'
+import { SummaryTile } from '../../components/admin'
 
 const DashboardPage: NextPage = () => {
   return (
@@ -9,7 +21,55 @@ const DashboardPage: NextPage = () => {
       subTitle="Estadísticas generales"
       icon={<DashboardOutlined />}
     >
-        <h3>Hola mundo</h3>
+      <Grid container spacing={2}>
+        <SummaryTile
+          title={1}
+          caption="Ordenes totales"
+          icon={<CreditCardOutlined color="secondary" sx={{ fontSize: 40 }} />}
+        />
+        <SummaryTile
+          title={1}
+          caption="Ordenes pagadas"
+          icon={<AttachMoneyOutlined color="success" sx={{ fontSize: 40 }} />}
+        />
+        <SummaryTile
+          title={1}
+          caption="Ordenes pendientes"
+          icon={<CreditCardOffOutlined color="error" sx={{ fontSize: 40 }} />}
+        />
+        <SummaryTile
+          title={4}
+          caption="Clientes"
+          icon={<GroupOutlined color="primary" sx={{ fontSize: 40 }} />}
+        />{' '}
+        <SummaryTile
+          title={5}
+          caption="Products"
+          icon={<CategoryOutlined color="warning" sx={{ fontSize: 40 }} />}
+        />
+        <SummaryTile
+          title={6}
+          caption="Sin Existencias"
+          icon={
+            <CancelPresentationOutlined color="error" sx={{ fontSize: 40 }} />
+          }
+        />
+        <SummaryTile
+          title={7}
+          caption="Inventario"
+          icon={
+            <ProductionQuantityLimitsOutlined
+              color="warning"
+              sx={{ fontSize: 40 }}
+            />
+          }
+        />
+        <SummaryTile
+          title={7}
+          caption="Actualización en:"
+          icon={<AccessTimeOutlined color="secondary" sx={{ fontSize: 40 }} />}
+        />
+      </Grid>
     </AdminLayout>
   )
 }
